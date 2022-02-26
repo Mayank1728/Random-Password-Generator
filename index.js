e09e6d03e42389e8dd380586c850e720ffe0d528
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-02-26 14:22:01 +0530

    first draft

---
+++ index.js
@@ -0,0 +1,4 @@
+const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const symbols = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
